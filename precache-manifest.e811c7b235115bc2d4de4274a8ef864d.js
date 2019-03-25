ef37daf174cf7a9d7e40ac7c8a7d92d4853e0517
at <artem.tereshchenko.it@gmail.com>
2019-03-26 00:47:17 +0300

    Updates

---
+++ precache-manifest.e811c7b235115bc2d4de4274a8ef864d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8f3d613d4b11a406254",
+    "url": "/react-todo-app/static/css/main.b6501e98.chunk.css"
+  },
+  {
+    "revision": "b8f3d613d4b11a406254",
+    "url": "/react-todo-app/static/js/main.b8f3d613.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.b1aeed9f.js"
+  },
+  {
+    "revision": "88c2d9a7a36dbcae1004",
+    "url": "/react-todo-app/static/js/2.88c2d9a7.chunk.js"
+  },
+  {
+    "revision": "55c6cb302aba35aae8d70439e4b5d146",
+    "url": "/react-todo-app/index.html"
+  }
+];
